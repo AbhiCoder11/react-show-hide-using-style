a53ccfc21612ae80ae409fe970c3622013b116e5
abhi@123 <abhishek@traveleasy.com>
2018-03-20 16:35:02 +0530

    My First commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+
+import Hello from './Hello';
+
+
+
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      loading:false,
+    }
+  }
+
+  getIt(){
+    console.log(this.state.loading)
+    this.setState({loading:!this.state.loading,})
+    console.log(this.state.loading)
+
+  }
+  
+  
+ 
+  render() {
+    return (
+      <div>
+        {this.state.loading?<Hello/>:null}
+      <button onClick={this.getIt.bind(this)} 
+      style={{ visibility: this.state.loading? 'hidden': 'visible'}}
+      >click here</button>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Hello.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Hello(){
+    return(
+        <i className="fa fa-spinner fa-spin black-text"  />
+    )
+}
+export default Hello;
